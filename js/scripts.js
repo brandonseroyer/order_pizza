ea7b574684c9bce77c22b4f8db341cd2d3880544
Brandon Seroyer <brandonseroyer@gmail.com>
2015-09-18 12:26:03 -0700

    first spec passes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-
+var Pizza = function(size, toppings, quanity) {
+  this.size = size;
+  this.toppings = toppings;
+  this.quanity = quanity;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -2,7 +2,7 @@ describe("Pizza", function() {
   it('creates a customized pizza', function() {
     var myPizza = new Pizza("Large", "Pepperoni", 1);
     expect(myPizza.size).to.equal("Large");
-    expect(myPizza.toppings).to.eql("Pepperoni");
+    expect(myPizza.toppings).to.equal("Pepperoni");
     expect(myPizza.quanity).to.equal(1);
   });
 });
