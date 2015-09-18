829bc49ed4b4ce0f46cab9a666eb5159d5cac838
Brandon Seroyer <brandonseroyer@gmail.com>
2015-09-18 12:20:08 -0700

    add first spec.

--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,8 @@
-
+describe("Pizza", function() {
+  it('creates a customized pizza', function() {
+    var myPizza = new Pizza("Large", "Pepperoni", 1);
+    expect(myPizza.size).to.equal("Large");
+    expect(myPizza.toppings).to.eql("Pepperoni");
+    expect(myPizza.quanity).to.equal(1);
+  });
+});
