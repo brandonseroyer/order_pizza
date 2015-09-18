619f27fb5e83cf03386999f19c1fcf074324d91d
Brandon Seroyer <brandonseroyer@gmail.com>
2015-09-18 13:25:36 -0700

    second spec pass; typos fixed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
-var Pizza = function(size, toppings, quanity) {
+function Pizza(size, topping, quanity) {
   this.size = size;
-  this.toppings = toppings;
+  this.topping = topping;
   this.quanity = quanity;
+}
+
+Pizza.prototype.totalValue = function() {
+var sizeValue = 0;
+  if (this.size === "X-Large") {
+    sizeValue = 20;
+  } else if (this.size === "Large") {
+    sizeValue = 16;
+  } else if (this.size === "Medium") {
+    sizeValue = 12;
+  } else {
+    sizeValue = 8;
+  }
+
+  var toppings = this.topping.split(" ");
+  var toppingValue = toppings.length;
+
+  var totalValue = toppingValue + sizeValue * (this.quanity);
+  return totalValue;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,12 +2,12 @@ describe("Pizza", function() {
   it('creates a customized pizza', function() {
     var myPizza = new Pizza("Large", "Pepperoni", 1);
     expect(myPizza.size).to.equal("Large");
-    expect(myPizza.toppings).to.equal("Pepperoni");
+    expect(myPizza.topping).to.equal("Pepperoni");
     expect(myPizza.quanity).to.equal(1);
   });
 
   it('calculates the order total', function() {
     var myPizza = new Pizza("Large", "Pepperoni", 1);
-    expect(myPizza.totalCost).to.equal(17);
+    expect(myPizza.totalValue()).to.equal(17);
   });
 });
