3d95a4a4b2f09b75d4dd6647953e68dd0f5303ed
Brandon Seroyer <brandonseroyer@gmail.com>
2015-09-18 12:32:28 -0700

    add second spec.

--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,9 @@ describe("Pizza", function() {
     expect(myPizza.toppings).to.equal("Pepperoni");
     expect(myPizza.quanity).to.equal(1);
   });
+
+  it('calculates the order total', function() {
+    var myPizza = new Pizza("Large", "Pepperoni", 1);
+    expect(myPizza.totalCost).to.equal(17);
+  });
 });
