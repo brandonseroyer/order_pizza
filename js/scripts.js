f1a9e058091309fa48c49f562d262a3fabe0e65d
Brandon Seroyer <brandonseroyer@gmail.com>
2015-09-22 21:05:08 -0700

    All Specs passing, app functional

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,10 @@ function Pizza(size, toppings, quantity) {
   this.quantity = quantity;
 }
 
+Pizza.prototype.addTopping = function(topping) {
+  this.toppings.push(topping);
+};
+
 Pizza.prototype.totalPrice = function() {
   var sizePrice = 0;
 
@@ -17,23 +21,27 @@ Pizza.prototype.totalPrice = function() {
       sizePrice += 8;
   }
 
-  if (this.toppings === "Pepperoni" || "Sausage" || "Bacon" || "Salami" || "Ham") {
-      sizePrice += 2;
-  }  else if (this.toppings === "Green Pepper" || "Mushroom" || "Onion" || "Black Olive") {
-      sizePrice += 1;
+  for(var index=0; index<this.toppings.length; index++) {
+    if (this.toppings[index] === "Chicken" || this.toppings[index] === "Pepperoni" || this.toppings[index] === "Sausage" || this.toppings[index] === "Bacon" || this.toppings[index] === "Salami" || this.toppings[index] === "Ham") {
+        sizePrice += 2;
+    }  else if (this.toppings[index] === "Green Pepper" || this.toppings[index] === "Mushroom" || this.toppings[index] === "Onion" || this.toppings[index] === "Black Olive") {
+        sizePrice += 1;
+    }
   }
 
 
-    var totalPrice = (sizePrice * (this.quantity));
-    return totalPrice;
+  var totalPrice = sizePrice * this.quantity;
+
+  return totalPrice;
   }
 
-  $(document).ready(function() {
-    var clearForm = function() {
-      $('input:checkbox').removeAttr('checked');
-      $('input:radio').removeAttr('checked');
-    };
 
+  var clearForm = function() {
+    $('input:checkbox').removeAttr('checked');
+    $('input:radio').removeAttr('checked');
+  };
+
+  var constructPage = function() {
     var mySize = ["Small", "Medium", "Large", "X-Large"];
     var myQuantity = [1, 2, 3, 4, 5, 10];
 
@@ -44,29 +52,36 @@ Pizza.prototype.totalPrice = function() {
     myQuantity.forEach(function(quantity) {
       $("select#quantity-list").append("<option value='" + quantity + "'><span class='chosen-quantity'>" + quantity + "</span></option>");
     });
-    var pizzaToppings = [];
-   $(this).find("input[name=toppings]:checked").each(function() {
-     pizzaToppings.push($(this).val());
-   });
+  }
+
+
+  $(document).ready(function() {
+
+    clearForm();
+    constructPage();
+
     $("form#user-info").submit(function(event) {
 
-    var pizzaSize = $("#size-list option:selected").text();
-    var pizzaQuantity = parseInt($("#quantity-list option:selected").val());
-    var myPizza = new Pizza(pizzaSize, parseInt(pizzaToppings.length), pizzaQuantity);
-    var myPrice = myPizza.totalPrice();
+      var pizzaToppings = [];
+      $(this).find("input[name=toppings]:checked").each(function() {
+        pizzaToppings.push($(this).val());
+      });
+      var pizzaSize = $("#size-list option:selected").text();
+      var pizzaQuantity = parseInt($("#quantity-list option:selected").val());
 
+      var myPizza = new Pizza(pizzaSize, pizzaToppings, pizzaQuantity);
+      var myPrice = myPizza.totalPrice();
 
-    $(".sizeChoice").text(pizzaSize);
-    $(".toppingsChoice").text(pizzaToppings);
-    $(".quantityChoice").text(pizzaQuantity);
-    $(".total-price").text(myPrice);
+      $(".sizeChoice").text(pizzaSize);
+      $(".toppingsChoice").text(pizzaToppings);
+      $(".quantityChoice").text(pizzaQuantity);
+      $(".total-price").text(myPrice);
 
+      $("select#size-list").val("");
+      $("select#pizza-toppings").val("");
+      $("select#quantity-list").val("");
 
-    $("select#size-list").val("");
-    $("select#pizza-toppings").val("");
-    $("select#quantity-list").val("");
-
-    $("#result").show();
+      $("#result").show();
       event.preventDefault();
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,14 @@
 describe("Pizza", function() {
   it('creates a customized pizza order', function() {
-    var myPizza = new Pizza("Large", "Pepperoni", 1);
-    expect(myPizza.size).to.equal("Large");
-    expect(myPizza.toppings).to.equal("Pepperoni");
-    expect(myPizza.quantity).to.equal(1);
+    var testPizza = new Pizza("Large", [], 1);
+    expect(testPizza.size).to.equal("Large");
+    expect(testPizza.toppings).to.eql([]);
+    expect(testPizza.quantity).to.equal(1);
   });
 
   it('calculates the total price of the customized order', function() {
-    var myPizza = new Pizza("Large", "Pepperoni", 1);
-    expect(myPizza.totalPrice()).to.equal(18);
+    var testPizza = new Pizza("Large", [], 1);
+    testPizza.addTopping("Pepperoni");
+    expect(testPizza.totalPrice()).to.equal(18);
   });
 });
